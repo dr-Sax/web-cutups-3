f086037e29473148b2f47c82ad702fcebcad0670
dr-Sax <nicoromano2000@gmail.com>
2024-10-22 15:57:42 -0400

    control for vertex shape elements

--- sketch.js
+++ sketch.js
@@ -1,35 +1,51 @@
-// A2Z F17
-// Daniel Shiffman
-// http://shiffman.net/a2z
-// https://github.com/shiffman/A2Z-F17
-
-// This is the content script for the extension
-
-// It creates a canvas overlay on a web page
-
-// p5 can not be executed the normal "global" way
-// Instead a sketch instance has to be manually created
-// This is done with the closure below
 
 
 var sketch = function(p5) {
+  var n_gon_array = [];
+  var new_shape = false;
 
     // The setup function
     p5.setup = function() {
-      console.log('p5 running');
       // Make a canvas that covers the HTML document up to 2000 pixels high
-      var h = p5.constrain(document.body.clientHeight, 0, 2000);
+      //var h = p5.constrain(document.body.clientHeight, 0, 2000);
+      h = 2000;
       var c = p5.createCanvas(p5.windowWidth, h);
+      c.style("z-index: 2");
+      c.style(`width: ${p5.windowWidth}px`);
+      c.style(`height: ${h}px`);
+      p5.strokeWeight(10);
+      p5.stroke('orange');
       // Position it
       c.position(0,0);
       p5.clear();
     }
   
     // Draw lines if the user drags the mouse
-    p5.mousePressed= function() {
-      p5.stroke(0);
-      p5.strokeWeight(4);
-      p5.point(p5.mouseX, p5.mouseY);
+    p5.keyPressed = function() {
+      
+      if (p5.key == 's'){
+        new_shape = true;
+        this.t = p5.beginShape(p5.POINTS);
+
+        window.addEventListener('click', event => {
+          if (event.target.matches('video')) {
+            event.stopPropagation();
+          }
+        }, true);
+
+      }
+
+      else if (p5.key == 'e'){
+        new_shape = false;
+        p5.endShape();
+        n_gon_array.push(this.t);
+      }
+    }
+
+    p5.mousePressed = function (){
+      if (new_shape){
+        p5.vertex(p5.mouseX, p5.mouseY);
+      }
     }
   
   }
